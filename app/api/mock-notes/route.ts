4d6bda8b54cd35c34f0e860cf599c8cb8f39b705
ArulNanjundan <arulnanjundan@gmail.com>
2025-06-12 17:26:59 +0530

    Add files via upload

---
+++ app/api/mock-notes/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const mockNotes = [
+  {
+    id: '1',
+    title: 'Arulmoorthy Nanjundan',
+    content: 'B.E Computer Science and Engineering\nCGPA:7.89',
+  },
+  {
+    id: '2',
+    title: 'Projects',
+    content: 'Exam Registration\nE-Commerce UI Deign\nQuiz using JavaFX',
+  },
+];
+
+export async function GET(req: NextRequest) {
+  return NextResponse.json({ notes: mockNotes });
+}
